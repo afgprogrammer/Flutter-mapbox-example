9384911f21bfecb783516cb68b7978a4c6e386bf
Mohammad Rahmani <rto1680@gmail.com>
2025-04-19 21:15:48 +0430

    Add app constants

---
+++ lib/app/constants.dart
@@ -0,0 +1,11 @@
+class AppConstants {
+  // For the flutter community
+  // This is a sample app to demonstrate the usage of Mapbox Maps in Flutter.
+  // The app is not intended for production use and is only for educational purposes.
+  // You can use this app as a reference to build your own app using Mapbox Maps in Flutter.
+  static const String appName = 'Flutter Mapbox Example';
+  static const String appVersion = '1.0.0';
+  static const String appUrl = 'https://afgrogrammer.com';
+  static const String mapboxToken = '';
+  static const String mapboxStyleID = "";
+}
