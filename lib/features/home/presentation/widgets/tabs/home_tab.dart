6e93b7a8853c7f5d363995d670539694f7cc890f
Mohammad Rahmani <rto1680@gmail.com>
2025-04-19 21:15:25 +0430

    Add basic map

--- lib/features/home/presentation/widgets/tabs/home_tab.dart
+++ lib/features/home/presentation/widgets/tabs/home_tab.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mapbox_example/app/constants.dart';
 import 'package:mapbox_maps_flutter/mapbox_maps_flutter.dart';
 
 class HomeTab extends StatefulWidget {
@@ -10,16 +11,29 @@ class HomeTab extends StatefulWidget {
 
 class _HomeTabState extends State<HomeTab> {
   late MapboxMap mapboxMap;
+
+  String mapboxToken = AppConstants.mapboxToken;
+
+  Future<void> _onMapCreated(MapboxMap createdMap) async {
+    MapboxOptions.setAccessToken(mapboxToken);
+    mapboxMap = createdMap;
+  }
   
   @override
   Widget build(BuildContext context) {
     return Container(
       color: Colors.white,
-      child: Center(
-        child: Text(
-          "Home Tab",
-          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+      child: MapWidget(
+        key: const ValueKey("mapWidget"),
+        cameraOptions: CameraOptions(
+          center: Point(coordinates: Position(-122.433135, 37.785160)),
+          zoom: 14,
         ),
+        styleUri: AppConstants.mapboxStyleID,
+        onMapCreated: _onMapCreated,
+        // onTapListener: (MapContentGestureContext c) {
+        //   _addMarkerToMap(c.point.coordinates.lng, c.point.coordinates.lat);
+        // },
       ),
     );
   }
